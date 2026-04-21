a27ab24c5f43294083ec741cfe3db1c706ea4965
aimnomr <aimnomr2202@gmail.com>
2026-04-21 17:39:40 +0800

    Disable Multiple Robot List View

--- src/components/StackedPage.jsx
+++ src/components/StackedPage.jsx
@@ -1,8 +1,8 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { navLinks } from '../pages/config/routes'
-import Lists from './List'
-import { robotList } from '../pages/config/robots'
+// import Lists from './List'
+// import { robotList } from '../pages/config/robots'
 import { conditionalHelper } from '../helper/conditionalHelper'
 
 export default function StackedPages() {
