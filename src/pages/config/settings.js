8732faaed79f51e25dc41557a0917fcbb88883f7
aimnomr <aimnomr2202@gmail.com>
2026-04-23 00:36:49 +0800

    Removed unused files

--- src/pages/config/settings.js
+++
@@ -1,6 +0,0 @@
-export const settings = {
-  1: { label: "Map View",      showTitle: true },
-  2: { label: "Camera View",   showTitle: true },
-  3: { label: "AMCL Pose",     showTitle: true },
-  4: { label: "Robot Control", showTitle: true },
-}
