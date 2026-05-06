6a7c94aa8d1f7f7738424c996953fba3486f2d19
aimnomr <aimnomr2202@gmail.com>
2026-05-06 22:00:03 +0800

    Robot Cancel Nav

--- src/components/GoalSelector.jsx
+++ src/components/GoalSelector.jsx
@@ -2,7 +2,7 @@ import { mockLocationList } from '../pages/config/mock-data'
 import { useMoveBase } from '../hooks/ROS/useMoveBase'
 
 function GoalSelector() {
-    const { publish } = useMoveBase()
+    const { publish, cancel } = useMoveBase()
 
     return (
         <div className="bg-gray-800 rounded-lg p-4">
@@ -20,6 +20,15 @@ function GoalSelector() {
                         </span>
                     </button>
                 ))}
+                <button
+                    onClick={() => cancel()}
+                    className="flex items-center justify-between px-4 py-2 bg-indigo-600 hover:bg-indigo-500 text-white rounded-lg transition-colors"
+                >
+                    <span>Cancel Nav</span>
+                    <span className="text-sm text-indigo-200">
+                        Stop
+                    </span>
+                </button>
             </div>
         </div>
     )
